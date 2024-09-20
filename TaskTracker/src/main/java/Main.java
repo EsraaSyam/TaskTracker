ccb2bb26dd2efb571ad6fc2700121ed93cacb576
EsraaSyam <e294014@gmail.com>
2024-09-20 20:58:24 +0300

    add Task class and test package gson

---
+++ TaskTracker/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        if(args.length < 1){
+            System.out.println("command not found");
+            return;
+        }
+
+        System.out.println("found");
+
+
+    }
+}
---
+++ TaskTracker/src/main/java/Task.java
@@ -0,0 +1,50 @@
+import java.time.LocalDateTime;
+public class Task {
+    private int cnt = 1;
+    private int id;
+    private String description;
+    private String status; // (todo, in-progress, done)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(String description){ // constructor
+        this.id = cnt++;
+        this.description = description;
+        this.status = "todo";
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+    // getter , setter
+
+    public int getId(){
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt(){
+        return updatedAt;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
