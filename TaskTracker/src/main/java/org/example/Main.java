c2bef89fff2abfba3a8f797799ead5d787bd9c1c
EsraaSyam <e294014@gmail.com>
2024-10-08 20:27:37 +0300

    fix a bug

--- TaskTracker/src/main/java/org/example/Main.java
+++ TaskTracker/src/main/java/org/example/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args){
         TaskManager t = new TaskManager();
         if(args.length < 1){
-            System.out.println("must provide operation");
+            System.out.println("must provide operation, use help to see all operations");
             return;
         }
 
