b75852bd604f164b4b422e40b1b229327eaaaf2a
EsraaSyam <e294014@gmail.com>
2024-10-08 20:19:57 +0300

    fix a bug in add task

--- TaskTracker/task-cli.sh
+++ TaskTracker/task-cli.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-mvn exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="$*"
+command=$1
+
+if [ "$command" == "add" ]; then
+    shift  
+    description="$*"  
+    mvn exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="add \"$description\""
+else
+    # Handle other commands (you can add more conditions here)
+    mvn exec:java -Dexec.mainClass="org.example.Main" -Dexec.args="$*"
+fi
